d0189eb7994488aeeb8e2e5c828ee746c09bbcad
Abdellah El Morabit <nsrddyn@gmail.com>
2025-03-19 16:06:47 +0100

    remove the previous containters to make it more ma,ageable for my self

--- synf-ui/src/views/login/login.tsx
+++ synf-ui/src/views/login/login.tsx
@@ -28,11 +28,28 @@ export function loginForms() {
 }
 
 function loginPage() {
-	return (
-		<>
-			{loginForms()}
-		</>
-	);
+    return (
+        <>
+
+            <section className="signup">
+                <form>
+                    <input type="text" name="txt" placeholder="User name" required/>
+                    <input type="email" name="email" placeholder="Email" required/>
+                    <input type="number" name="broj" placeholder="BrojTelefona" required/>
+                    <input type="password" name="pswd" placeholder="Password" required/>
+                    <button className={"loginButton"}>Sign up</button>
+                </form>
+            </section>
+
+            <section className="login">
+                <form>
+                    <input type="email" name="email" placeholder="Email" required/>
+                    <input type="password" name="pswd" placeholder="Password" required/>
+                    <button className={"loginButton"}>Login</button>
+                </form>
+            </section>
+        </>
+    );
 }
 
 export default loginPage;
