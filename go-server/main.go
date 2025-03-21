78f7c7949bae8d9fe682f625f2d745e70241736d
nasrlol <nsrddyn@gmail.com>
2025-03-21 03:28:49 +0100

    starting with the design of the code

--- go-server/main.go
+++ go-server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
-func main(){
-    fmt.Println("Hello World")
+type device struct {
+	device_id   int
+	device_name string
+}
+
+type stats struct {
+	cpu_frequency float64
+	cpu_temp      float64
+	cpu_name      string
+
+    ram_name      string
+    ram_used      int
+    ram_free      int
+    ram_total     int
+
+}
+
+func getSystemInformation() device {
+	return device{}
+}
+
+func main() {
+	newCpu := getSystemInformation()
+	fmt.Println(newCpu)
 }
