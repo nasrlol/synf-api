c1349e0b23441338f694b41ea05bfb1157694bab
nasrlol <nsrddyn@gmail.com>
2025-03-21 17:33:57 +0100

    worked on designing the structs for the data, now trying to setup a connection to the mariadb database

--- go-server/main.go
+++ go-server/main.go
@@ -1,31 +1,112 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
+
+	"net/http"
+	"encoding/json"
+
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+
+	"github.com/joho/godotenv"
+	"os"
 )
 
+type userInformation struct
+{
+	userID uint16 `json:"id"`
+	userName string `json:"user_name"` 
+	userRole bool `json:"user_role"`
+	userEmail string `json:"user_email"`
+	userEmailVerified bool `json:"is_email_verified"`
+}
+
 type device struct {
-	device_id   int
-	device_name string
+	deviceID   uint16 `json:"device_id"` 
+	deviceName string `json:"device_name"`
+	deviceStatus bool `json:"is_loggin"`   
 }
 
-type stats struct {
-	cpu_frequency float64
-	cpu_temp      float64
-	cpu_name      string
+type CPUstats struct {
+	cpuID	uint16 `json:cpu_id`	
+	cpuName	string `json:"cpu_name"`
+	cpuFreq	float64 `json:"cpu_clock_speed"` 
+	cpuTemp float64
+}
 
-    ram_name      string
-    ram_used      int
-    ram_free      int
-    ram_total     int
+type GPUstats struct
+{
+	gpuID uint16 `json:"gpu_id"`
+	gpuName string `json:"gpu_name"`
+	gpuFreq float64 `json:"gpu_clock_speed"`
+	gpuTemp float64 `json:"gpu_temp"`
 
 }
 
-func getSystemInformation() device {
-	return device{}
+type RAMstats struct
+{
+	ramID		uint64  `json:"ram_id"` 
+	ramName      string `json:"ram_name"`
+	ramUsed      float64 `json:"ram_used"`
+	ramTotal     float64 `json:"ram_total"`
 }
+
+type DISKstats struct
+{
+	diskID uint64 `json:"disk_id"`
+	diskName string `json:"disk_name"`
+	disktemp float64 `json:"disk_temp"`
+	diskSize int `json:"diskName"`
+}
+
+type LOGIN struct 
+{
+	user string
+	pass string
+	ip   string 
+	port string
+}
+
+func loadCredentials() LOGIN{
+
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("Error loading .env file")
+	}
+
+	user := os.Getenv("DATABASE_USER")
+	pass := os.Getenv("DATABASE_PASSWORD")
+	ip := os.Getenv("DATABASE_IP")
+	port := os.Getenv("DATABASE_PORT")
+
+	return LOGIN{
+		user: user,
+		pass: pass,
+		ip: ip,
+		port: port,
+	}
+}
+
+func setDeviceInformation() device {
+	// Get the system information from the database
+
+	newDevice := device{
+		deviceID: 1,
+		deviceName: "", 
+		deviceStatus: false,
+	}
+	return newDevice
+}
+
+func updateUserInformatio()  {
+
+
+	sql.Open("mysql", "`{credentials.user}`:root@tcp(host:port)/dbname")
+}
+
 
 func main() {
-	newCpu := getSystemInformation()
-	fmt.Println(newCpu)
+	fmt.Println("getting system data...")
 }
