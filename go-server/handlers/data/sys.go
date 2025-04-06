c1e4719ebec879f5a3d4539b485cc0b1f1d9204d
Abdellah El Morabit <nsrddyn@gmail.com>
2025-04-06 17:40:31 +0200

    getting cpu information from the device and printing it out in go, just need to parse it, and sent it over http, using websockets

--- go-server/handlers/data/sys.go
+++ go-server/handlers/data/sys.go
@@ -3,8 +3,15 @@ package sys
 
 import (
 	"fmt"
+	"os/exec"
 )
 
 func Sys(){
 	fmt.Println("getting system data")
+	cpuTemperature, err := exec.Command("../../../synf-sys/cpu").Output()
+	if err != nil{
+		fmt.Println("cant get cpu information")
+		return
+	}
+	fmt.Println(cpuTemperature)
 }
