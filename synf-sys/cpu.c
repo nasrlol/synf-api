9c469453611952d930c4716317fc0b70409b4b1a
Abdellah El Morabit <nsrddyn@gmail.com>
2025-04-06 13:49:34 +0200

    [FEATURE] CPU INFORMATION
    testing steel needed

--- synf-sys/cpu.c
+++ synf-sys/cpu.c
@@ -1,35 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-char *cpu_name()
+#define MAXC 1024
+
+void cpu_name(void);
+void cpu_temperature(unsigned short delay);
+void cpu_frequency(void);
+
+int main(void)
 {
-    char *name;
-    // /proc/cpuinfo gives a lot of information, i have to parse it and get the model name of the cpu and return that
-    FILE *pf = fopen("proc/cpuinfo", "r");
-    if(!pf)
+    cpu_frequency();
+    cpu_name();
+    cpu_temperature(1);
+
+    return 0;
+}
+
+void cpu_name(void)
+{
+    int buffer_size = 256;
+    char cpu_name[buffer_size];
+
+    FILE *fp = fopen("/proc/cpuinfo", "r");
+    if (!fp)
+        printf("can't open /proc/cpuinfo");
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp))
     {
-        // error handling, in case of not being to open /proc/cpuinfo
-        printf("error reading the cpu frequency");
+        if (strncmp(line, "model name", 10) == 0)
+        {
+            char *colon = strchr(line, ':');
+            if (colon)
+            {
+                snprintf(cpu_name, buffer_size, "%s", colon + 2);
+                cpu_name[strcspn(cpu_name, "\n")] = 0;
+                fclose(fp);
+            }
+        }
     }
 
-
-    return name;
+    fclose(fp);
+    snprintf(cpu_name, buffer_size, "Unknown");
+    printf("%s", cpu_name);
 }
-int cpu_temperature(int delay)
+
+void cpu_temperature(unsigned short delay)
 {
-    while (1)
+    while (delay > 0)
     {
         sleep(delay);
         FILE *pf = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
         // error handling in case of not being able to open /sys/class/thermal/..
         if (!pf)
-        {
             printf("error reading /proc/cpuinfo");
-            return 1;
-        }
 
-        char buffer[1024];
+        char buffer[MAXC];
         while (fgets(buffer, sizeof(buffer), pf))
         {
             int a = atoi(buffer);
@@ -38,40 +66,25 @@ int cpu_temperature(int delay)
         }
         fclose(pf);
     }
-    return 0;
+    delay--;
 }
 
-int cpu_frequency()
+void cpu_frequency(void)
 {
     while (1)
     {
         FILE *pf = fopen("/proc/cpuinfo", "r");
         // error handling in case of not being able to open the file
         if (!pf)
-        {
             printf("error reading /proc/cpuinfo");
-        }
 
-        char buffer[1024];
+        char buffer[MAXC];
         while (fgets(buffer, sizeof(buffer), pf))
         {
             int a = atoi(buffer);
             printf("CPU FREQ: %d\n", a);
         }
-        fclosee(pf);
-    }
-    return 0;
-}
 
-int main(int argc, char **argv)
-{
-    if (argc > 1)
-    {
-        cpu_temperature(atoi(argv[1]));
+        fclose(pf);
     }
-    else
-    {
-        cpu_temperature(1);
-    }
-    return 0;
 }
