2fcdaa71708aa75d97cd776c4b40b197fe9991eb
Abdellah El Morabit <nsrddyn@gmail.com>
2025-04-06 20:16:34 +0200

    implemented the functions for cpu readings in go, now just need to send them over http

--- go-server/handlers/data/sys.go
+++ go-server/handlers/data/sys.go
@@ -1,17 +1,39 @@
 package sys
 
-
 import (
 	"fmt"
 	"os/exec"
 )
 
-func Sys(){
-	fmt.Println("getting system data")
-	cpuTemperature, err := exec.Command("../../../synf-sys/cpu").Output()
+func CpuTemperature(){
+	fmt.Println("Getting CPU temperature")
+	cpuTemperature, err := exec.Command("../../../synf-sys/cpu", "temp").Output()
 	if err != nil{
-		fmt.Println("cant get cpu information")
+		fmt.Println("can't get cpu temperature")
 		return
 	}
 	fmt.Println(cpuTemperature)
+}
+
+func CpuName(){
+	fmt.Println("Getitng the CPU Name")
+	cpuName, err := exec.Command("/synf-sys/cpu", "name").Output()
+	if err != nil{
+		fmt.Println("can't get the cpu name")
+	}
+	fmt.Println(cpuName)
+}
+
+func CpuFrequency(){
+	fmt.Println("Getting the CPU frequency")
+	cpuFreq, err := exec.Command("/synf-sys/cpu", "freq").Output()
+		if err != nil{
+		fmt.Println("can't get the cpu name")
+	}
+	fmt.Println(cpuFreq)
+}
+
+func Sys(){
+	CpuTemperature()
+	CpuName()
 }
