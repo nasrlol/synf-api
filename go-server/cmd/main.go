a6baee8d7a613c9bfd67741804c833dc5c100cca
Abdellah El Morabit <nsrddyn@gmail.com>
2025-04-08 02:08:22 +0200

    testing reading the cpu name and printing it out in stdout

--- go-server/cmd/main.go
+++ go-server/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	sys "synf/handlers/data"
 	"synf/server"
 )
 
@@ -9,6 +10,7 @@ func main() {
 
 	// clear terminal screen ASCII sequence
 	fmt.Print("\033[H\033[2J")
+	fmt.Print(sys.CpuName())
 
 	server.RawConnect("127.0.0.1", "5000")
 	server.RegistrationEndpoint()
