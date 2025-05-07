de4b290a884fb0c20c0f428f771197315fa5a739
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-07 22:17:51 +0200

    getting the memory usage in bytes, then calculating it to gigabytes, still having some small issues with that though

--- system/source/ram.c
+++ system/source/ram.c
@@ -13,27 +13,64 @@
  *         Author:  nasr 
  *   Organization:  synf 
  *
- * =====================================================================================
- */
+ * =====================================================================================*/
+
+// OSX
+#ifdef __APPLE__
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#include <math.h>
 
+#define CONVERT_BYTES_TO_GIGABYTES 107374182   
 
-struct cpu {
-    char* memName;
-    int memSize;
-};
+typedef struct {
+  char* memName;
+  int memSize;
+} ram;
 
-int getFreeMem(void);
-int getTotalMem(void);
+long getTotalMem(void);
 int getMemFreq(void);
+long getMemoryUsage(void);
+
+
+long getMemoryUsage(void) 
+{
+
+  struct rusage usage;
+  if(0 == getrusage(RUSAGE_SELF, &usage))
+    return usage.ru_maxrss / CONVERT_BYTES_TO_GIGABYTES ; 
+  else
+    return 0;
+}
+
+long getTotalMem(void){
+
+  int i, mib[4];
+  size_t len;
+
+  len = 4;
+
+
+  return 0;
+}
 
 int main()
 {
-    return 0;
+  printf("%lu", getMemoryUsage());
+  return 0;
 }
 
-int getFreeMem(){
-    printf("hello world");
-    return 0;
+
+#endif
+
+#ifdef __gnu_linux__
+
+int main()
+{
+
+  return 0;
 }
+#endif
