b9bbf4bf9931188462cfcae43cb139759a5d8a82
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-09 23:05:53 +0200

    api routes boilerplate

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/cpu", CpuHandler)
+	http.HandleFunc("/gpu", GpuHandler)
+	http.HandleFunc("/ram", RamHandler)
+	http.HandleFunc("/disk", DiskHandler)
+	http.HandleFunc("/gendev", GenDevHandler)
+}
+
+func CpuHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GpuHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func RamHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DiskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GenDevHandler(w http.ResponseWriter, r *http.Request) {
+
+}
