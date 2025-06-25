f2ba0849dbd468aed5c2eef86d693b23f418bb1b
Abdellah El Morabit <nsrddyn@gmail.com>
2025-06-26 00:44:08 +0200

    syntax error fix and added a condition for creating the user table

--- internal/migrations/20250624124146_user.go
+++ internal/migrations/20250624124146_user.go
@@ -13,15 +13,14 @@ func init() {
 
 func upUser(ctx context.Context, tx *sql.Tx) error {
 
-	_, err := tx.Exec(`CREATE TABLE users 
+	_, err := tx.Exec(`CREATE TABLE IF NOT EXISTS users 
 						(id INTEGER AUTO_INCREMENT PRIMARY KEY,
 						name VARCHAR(255) NOT NULL,
 						password VARCHAR(255) NOT NULL,
 						role VARCHAR(255) NOT NULL,
 						email VARCHAR(255) UNIQUE NOT NULL,
-						verified TINYINT(1)) DEFAULT 0`) 
+						verified TINYINT(1) DEFAULT 0`) 
 
-	// creating a users table that has an id as a 
 	// creating a users table that has an id as a 
 	// primary key, a username, a password, encrypted
 	// a role (admin, user), a unique email, and a verified status 
