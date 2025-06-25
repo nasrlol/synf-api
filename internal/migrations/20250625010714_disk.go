276432e3376ab70eb66c1ea46529b86515502200
nasrlol <nsrddyn@gmail.com>
2025-06-25 03:10:14 +0200

    disk migraiotns

---
+++ internal/migrations/20250625010714_disk.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upDisk, downDisk)
+}
+
+func upDisk(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec(`CREATE TABLE disks (
+											id INTEGER PRIMARY KEY AUTO_INCREMENT,
+											name VARCHAR(255) NOT NULL,
+											size VARCHAR(255) NOT NULL,
+										)`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func downDisk(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec(`DROP TABLE IF EXISTS disks`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
