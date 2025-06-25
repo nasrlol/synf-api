cd93997fc39ab51dd107adf71d7a84721e3d3958
nasrlol <nsrddyn@gmail.com>
2025-06-25 12:19:56 +0200

    removed trailing comma

--- internal/migrations/20250625010714_disk.go
+++ internal/migrations/20250625010714_disk.go
@@ -15,7 +15,7 @@ func upDisk(ctx context.Context, tx *sql.Tx) error {
 	_, err := tx.Exec(`CREATE TABLE disks (
 											id INTEGER PRIMARY KEY AUTO_INCREMENT,
 											name VARCHAR(255) NOT NULL,
-											size VARCHAR(255) NOT NULL,
+											size VARCHAR(255) NOT NULL
 										)`)
 	if err != nil {
 		return err
