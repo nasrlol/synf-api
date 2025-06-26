6c51d8891673190b12cd2f9eef22c1d20ef4b899
Abdellah El Morabit <nsrddyn@gmail.com>
2025-06-26 16:36:28 +0200

    update

--- internal/database/db.go
+++ internal/database/db.go
@@ -72,6 +72,35 @@ func ConnectDB() (*sql.DB, error) {
 	return db, nil
 }
 
+func GetUser(userName string) (reg.UserInformation, error) {
+	var userInfo reg.UserInformation
+	query := "SELECT user.id FROM user WHERE name LIKE ?"
+
+	conn, err := db.ConnectDB()
+	if err != nil {
+		return reg.UserInformation{}, fmt.Errorf("failed to connect to the database")
+	}
+
+	row := conn.QueryRow(query, userName)
+	err = row.Scan(&userInfo.UserName)
+	if err != nil {
+		return reg.UserInformation{}, fmt.Errorf("there was an error in retrieving the information from the database")
+	}
+	return userInfo, nil
+}
+
+func CheckPass(pass string, data reg.UserInformation) bool {
+	query := "SELECT ? FROM ? WHERE ? LIKE ?"
+
+	conn, err := db.ConnectDB()
+	if err != nil {
+		return false
+	}
+
+	queryError := conn.QueryRow(query, data.UserID, "USER", data.UserPassword, pass)
+	return queryError == nil
+}
+
 // Stats returns the database statistics
 func Stats(db *sql.DB) sql.DBStats {
 	return db.Stats()
