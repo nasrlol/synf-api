2675fd492ea1d64074d2e5c1195a1a27e070d3d2
Abdellah El Morabit <nsrddyn@gmail.com>
2025-06-26 22:54:00 +0200

    completed the user migrations

--- internal/migrations/20250624124146_user.go
+++ internal/migrations/20250624124146_user.go
@@ -12,10 +12,27 @@ func init() {
 }
 
 func upUser(ctx context.Context, tx *sql.Tx) error {
+
+	_, err := tx.Exec(`CREATE TABLE users (
+											id INTEGER PRIMARY KEY AUTO_INCREMENT,
+											name VARCHAR(255) NOT NULL,
+											email VARCHAR(255) NOT NULL UNIQUE,
+											password VARCHAR(255) NOT NULL,
+											role VARCHAR(255) NOT NULL,
+											verified BOOLEAN NOT NULL
+										)`)
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func downUser(ctx context.Context, tx *sql.Tx) error {
-	// This code is executed when the migration is rolled back.
+
+	_, err := tx.Exec(`DROP TABLE IF EXISTS users`)
+	if err != nil {
+		return err
+	}
 	return nil
 }
