a1413029014dac5212f8032708def344597038ff
nasrlol <nsrddyn@gmail.com>
2025-07-02 23:05:14 +0200

    update disks table schema to change size type from VARCHAR to INTEGER

--- internal/migrations/20250625010714_disk.go
+++ internal/migrations/20250625010714_disk.go
@@ -15,7 +15,7 @@ func upDisk(ctx context.Context, tx *sql.Tx) error {
 	_, err := tx.Exec(`CREATE TABLE IF NOT EXISTS disks (
 											id INTEGER PRIMARY KEY AUTO_INCREMENT,
 											name VARCHAR(255) NOT NULL,
-											size VARCHAR(255) NOT NULL
+											size INTEGER NOT NULL
 										)`)
 	if err != nil {
 		return err
