5b54cf0b5e54457f8b067388a590f8d470e76415
nasrlol <nsrddyn@gmail.com>
2025-07-02 23:08:44 +0200

    add migration and reset migration functions with command-line support

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 	"embed"
 	"fmt"
+	"os"
 
 	"github.com/pressly/goose/v3"
 )
 
 var embedMigrations embed.FS
 
-func migrations() {
+func migrate() {
 	var db *sql.DB
 
 	goose.SetBaseFS(embedMigrations)
@@ -24,6 +25,34 @@ func migrations() {
 	}
 }
 
+func resetMigrations() {
+	var db *sql.DB
+
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("mysql"); err != nil {
+		panic(err)
+	}
+
+	if err := goose.Down(db, "migrations"); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	args := os.Args[1:]
+
 	fmt.Println("\033[H\033[2J")
+	if len(args) > 0 {
+		switch args[0] {
+		case "migrate":
+			migrate()
+		case "migrations":
+			migrate()
+		case "reset-migrations":
+			resetMigrations()
+		case "reset-migrate":
+			resetMigrations()
+		}
+	}
 }
