3141e3de6d45113b9018bea9f3699261b8578fa3
nasrlol <nsrddyn@gmail.com>
2025-07-02 23:06:57 +0200

    add Cpu function to execute commands and stream output

---
+++ internal/api/data/services/cpu_service.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"bufio"
+	"log"
+	"os/exec"
+)
+
+func Cpu(path string, parameter string) (<-chan string, string) {
+	outChan := make(chan string)
+
+	if parameter != "name" {
+		cmd := exec.Command(path, parameter)
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(stdout)
+
+		go func() {
+			defer close(outChan)
+			for scanner.Scan() {
+				outChan <- scanner.Text()
+			}
+
+			if err := scanner.Err(); err != nil {
+				log.Printf("scanner error %v\n", err)
+			}
+		}()
+		return outChan, parameter
+	}
+}
