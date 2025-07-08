1eeac0960e770da9e2c8f81485dd21d5db0fbd02
nasrlol <nsrddyn@gmail.com>
2025-07-08 09:31:27 +0200

    update http_routes.go to rename handlers for consistency and clarity

--- internal/api/routes/http_routes.go
+++ internal/api/routes/http_routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"net/http"
+
 	handlers "synf/internal/api/data/rest"
 )
 
 func InitRestRoutes() {
-	http.HandleFunc("/data/ram", ramHandler)
-	http.HandleFunc("/data/disk", diskHandler)
-	http.HandleFunc("/data/device", deviceHandler)
-	http.HandleFunc("/user/login", handlers.GetUser)
+	http.HandleFunc("/data/ram", RamHandler)
+	http.HandleFunc("/data/disk", DiskHandler)
+	http.HandleFunc("/data/device", DeviceHandler)
+	http.HandleFunc("/user/login", handlers.LoginHandler)
 	http.HandleFunc("/user/registration", handlers.CreateUser)
 }
