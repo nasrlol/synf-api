d5c7efae1a10d34c70283104afecae61e741a94e
nasrlol <nsrddyn@gmail.com>
2025-07-08 09:31:34 +0200

    refactor user_handler.go to improve user authentication flow and error handling

--- internal/api/data/rest/user_handler.go
+++ internal/api/data/rest/user_handler.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
+
 	"synf/internal/api/data/models"
 
 	"synf/internal/database"
@@ -14,85 +14,50 @@ import (
 )
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-type", "application/json")
 
 	var request struct {
-		"Email" string `json:"email"`
-		"Password" string `json:"password"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	conn, err := database.Connect()
+	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		w.WriteHeader(404)
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	query := `SELECT EXISTS (
-					SELECT 1
-					FROM USER
-					WHERE name LIKE ?
-					AND
-					email LIKE ?
-					AND
-					password LIKE ?)`
 
-	conn.Exec(query, request.Username, request)
+	query := `SELECT * FROM USER WHERE name LIKE ? AND	email LIKE ? AND password LIKE ?`
 
-	err := json.NewEncoder(w).Encode(data)
+	conn, _ := database.Connect()
+
+	var user models.UserInformation
+	var hashedPassword []byte
+
+	err = conn.QueryRow(query, request.Email, hashedPassword).Scan(&user.Id, &user.Name, &user.Email, &user.Password, &user.Role, &user.Verified)
+
+	switch {
+	case err == sql.ErrNoRows:
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	case err != nil:
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	hashedPassword, _ = bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(user.Password))
 	if err != nil {
 		return
 	}
+
+	user.Password = ""
+
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(user)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-
-	conn, err := database.Connect()
-	if err != nil {
-		w.WriteHeader(500)
-	}
-	if conn != nil {
-		defer func(conn *sql.DB) {
-			err := conn.Close()
-			if err != nil {
-				w.WriteHeader(500)
-			}
-		}(conn)
-	} else {
-		w.WriteHeader(500)
-	}
-
-	query := `INSERT INTO users (name, password, email, role) VALUES (?, ?, ?, ?)`
-	r.GetBody(models.UserInformation{UserName: })
-	_, err = conn.Exec(query, data.UserName, data.UserPassword, data.UserRole, data.UserEmail)
-	if err != nil {
-		w.WriteHeader(500)
-	}
-	var user models.UserInformation
-
-	err := json.NewDecoder(r.Body).Decode(&user)
-	fmt.Println("converting user information to json...")
-	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.UserPassword), bcrypt.DefaultCost)
-	fmt.Println("hashing the user password...")
-	if err != nil {
-		http.Error(w, "Error hashing password", http.StatusInternalServerError)
-		return
-	}
-
-	user.UserPassword = string(hashedPassword)
-
-	if value, err := insertUser(user); value != true && err != nil {
-		http.Error(w, "Failed to insert user", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]interface{}{
-		"message": "User registered successfully",
-	})
 }
