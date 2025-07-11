e8ebe93cc3ded298a28a84a41dacba6d106e9092
Abdellah El Morabit <nsrddyn@gmail.com>
2025-07-11 14:52:46 +0200

    feat: register and login operations for users

--- internal/api/data/rest/user_handler.go
+++ internal/api/data/rest/user_handler.go
@@ -14,8 +14,6 @@ import (
 )
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
-
 	var request struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
@@ -23,19 +21,23 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	query := `SELECT * FROM USER WHERE name LIKE ? AND	email LIKE ? AND password LIKE ?`
+	query := `SELECT * FROM USER WHERE email = ?`
 
-	conn, _ := database.Connect()
+	conn, err := database.Connect()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+
+	defer database.Close(conn)
 
 	var user models.UserInformation
-	var hashedPassword []byte
 
-	err = conn.QueryRow(query, request.Email, hashedPassword).Scan(&user.Id, &user.Name, &user.Email, &user.Password, &user.Role, &user.Verified)
+	err = conn.QueryRow(query, request.Email).Scan(&user.Id, &user.Name, &user.Email, &user.Password, &user.Role, &user.Verified)
 
 	switch {
 	case err == sql.ErrNoRows:
@@ -47,9 +49,9 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	hashedPassword, _ = bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
-	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(user.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password))
 	if err != nil {
+		http.Error(w, "Invalid credentials", http.StatusBadRequest)
 		return
 	}
 
@@ -60,4 +62,46 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var request struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	conn, err := database.Connect()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+
+	defer database.Close(conn)
+
+	query := `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`
+
+	_, err = conn.Exec(query, request.Name, request.Email, hashedPassword)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	database.Close(conn)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
 }
